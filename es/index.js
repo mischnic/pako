515fc24c65e4942d5968aa59b950fc6144085f8a
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-10-07 10:00:07 +0200

    Optimize

--- es/index.js
+++ es/index.js
@@ -1,3 +1,3 @@
-export * from "./deflate";
-export * from "./inflate";
+export { Deflate, deflate, deflateRaw, gzip } from "./deflate";
+export { Inflat, inflate, inflateRaw, gunzip } from "./inflate";
 export * from "./zlib/constants";
